e84d83d7bb582e6cfc1af51a5d2e4b6329214be2
naveenverma1 <01naveenv@gmail.com>
2020-09-17 00:26:22 +0530

    webhooks updated

--- charge.js
+++ charge.js
@@ -1,77 +1,94 @@
-const express = require("express");
-const app = express();
-const {
-  resolve
-} = require("path");
-const port = 3000
+// const express = require('express');
+// const app = express();
+// const {
+//   resolve
+// } = require('path');
+// const port = 3000
 
 
+// const bodyParser = require('body-parser')
+// const exphbs = require('express-handlebars')
+// // Set your secret key. Remember to switch to your live secret key in production!
+// // See your keys here: https://dashboard.stripe.com/account/apikeys
+// const stripe = require('stripe')('sk_test_51HNXcdICR0NVfhA1jTSZ93s3hsPX8L1uQsDBeRkK9mkzUtn44srebA18czdMvBiJVo7infozLjVc0TCsBepQED7h003VRwixxQ');
 
-// Set your secret key. Remember to switch to your live secret key in production!
-// See your keys here: https://dashboard.stripe.com/account/apikeys
-const stripe = require('stripe')('sk_test_51HNXcdICR0NVfhA1jTSZ93s3hsPX8L1uQsDBeRkK9mkzUtn44srebA18czdMvBiJVo7infozLjVc0TCsBepQED7h003VRwixxQ');
+// // const customer = await stripe.customers.create({
+// //   email: 'paying.user@example.com',
+// //   source: source,
+// // });
 
-// const customer = await stripe.customers.create({
-//   email: 'paying.user@example.com',
-//   source: source,
+// app.engine('handlebars',exphbs({defaultLayout:'main'}))
+// app.set('view engine','handlebars');
+// //body-parser
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended:false}));
+// //set static folder
+
+// app.use(express.static('.'));
+// app.use(express.json());
+// app.get("/", (req, res) => {
+//   // Display checkout page
+//   const path = resolve("/indexn.html");
+//   res.sendFile(path);
+// });
+// //app.get("/", (request, response) => response.status(200).send("hello world"));
+
+// // Set your secret key. Remember to switch to your live secret key in production!
+// // See your keys here: https://dashboard.stripe.com/account/apikeys
+// //const stripe = require('stripe')('sk_test_51HNXcdICR0NVfhA1jTSZ93s3hsPX8L1uQsDBeRkK9mkzUtn44srebA18czdMvBiJVo7infozLjVc0TCsBepQED7h003VRwixxQ');
+// app.post("/charge", async (req, res) => {
+//   //source = req.body.
+// const amount =  2500;
+// stripe.customers.create({
+//   email:req.body.stripeEmail,
+//   source:req.body.stripeSource
+// }).then(customer=> stripe.charges.create({
+//   amount,
+//   currency:'eur',
+//   customer:customer.id,
+//   source :source
+// })).then(charge=> res.render('success'))
+//   //const { token, items } = req.body;
+//  // source =      \
+// //  source = req.body.stripeToken
+// //   name = req.body
+// //   email = req.body    
+// // JSON.stringify(source)
+// // JSON.stringify(name)
+// // JSON.stringify(email)
+// // console.log(typeof source)
+
+
+// // const customer = await stripe.customers.create({
+// //   email: 'khkb@gmail.com',
+  
+// //   source: source,
+
+  
+// // });
+
+// const  itenName = "Premium Script";
+
+// //   try {
+// //     const charge = await stripe.charges.create({
+// //       customer:customer.id,
+// //       amount: 1099,
+// //       currency: 'eur',
+// //       source: source
+// //     });
+// // var cjson = JSON.stringify(charge)
+// // console.log(cjson+hhh)
+// //     res.send(charge);
+// //     console.log(cjson+hhh)
+// //   } catch (e) {
+// //     // Handle "hard declines" e.g. insufficient funds, expired card, etc
+// //     // See https://stripe.com/docs/declines/codes for more
+// //     res.send({
+// //       error: e.message
+// //     });
+// //   }
 // });
 
-
-app.use(express.static("."));
-app.use(express.json());
-app.get("/", (req, res) => {
-  // Display checkout page
-  const path = resolve("/index.html");
-  res.sendFile(path);
-});
-//app.get("/", (request, response) => response.status(200).send("hello world"));
-
-// Set your secret key. Remember to switch to your live secret key in production!
-// See your keys here: https://dashboard.stripe.com/account/apikeys
-//const stripe = require('stripe')('sk_test_51HNXcdICR0NVfhA1jTSZ93s3hsPX8L1uQsDBeRkK9mkzUtn44srebA18czdMvBiJVo7infozLjVc0TCsBepQED7h003VRwixxQ');
-app.post("/charge", async (req, res) => {
-
-  //const { token, items } = req.body;
- // source =      \
- source = req.body.stripeToken
-  name = req.body
-  email = req.body    
-JSON.stringify(source)
-JSON.stringify(name)
-JSON.stringify(email)
-console.log(typeof source)
-
-
-const customer = await stripe.customers.create({
-  email: 'khkb@gmail.com',
-  
-  source: source,
-
-  
-});
-
-const  itenName = "Premium Script";
-
-  try {
-    const charge = await stripe.charges.create({
-      customer:customer.id,
-      amount: 1099,
-      currency: 'eur',
-      source: source
-    });
-var cjson = JSON.stringify(charge)
-console.log(cjson+hhh)
-    res.send(charge);
-    console.log(cjson+hhh)
-  } catch (e) {
-    // Handle "hard declines" e.g. insufficient funds, expired card, etc
-    // See https://stripe.com/docs/declines/codes for more
-    res.send({
-      error: e.message
-    });
-  }
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`)
+// })
---
+++ server.js
@@ -0,0 +1,89 @@
+// Replace if using a different env file or config
+//require("dotenv").config({ path: "./env" });
+const express = require("express");
+const app = express();
+const { resolve } = require("path");
+const bodyParser = require("body-parser");
+const stripe = require("stripe")('sk_test_51HNXcdICR0NVfhA1jTSZ93s3hsPX8L1uQsDBeRkK9mkzUtn44srebA18czdMvBiJVo7infozLjVc0TCsBepQED7h003VRwixxQ')
+const webhookSecret = 'whsec_OHqWX2tDNVQkWwCraQn0pkgrezk9BQWd';
+
+app.use(express.static('.'));
+// Use JSON parser for all non-webhook routes
+app.use((req, res, next) => {
+  if (req.originalUrl === "/webhook") {
+    next();
+  } else {
+    bodyParser.json()(req, res, next);
+  }
+});
+
+app.get("/", (req, res) => {
+    // Display checkout page
+    const path = resolve("./index.html");
+    res.sendFile(path);
+  });
+app.get("/success", (req, res) => {
+  const path = resolve("./success.html");
+  res.sendFile(path);
+});
+
+app.get('/checkout-session', async (req, res) => {
+  const session = await stripe.checkout.sessions.retrieve(req.query.id, {
+    expand: ['line_items']
+  });
+  res.json(session);
+});
+app.post('/create-checkout-session', async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    success_url: 'http://localhost:4242/success?id={CHECKOUT_SESSION_ID}',
+    cancel_url: 'http://localhost:4242/cancel',
+    payment_method_types: ['card'],
+    mode: 'subscription',
+    allow_promotion_codes: true,
+    line_items: [{
+      price: 'price_1HS1SqICR0NVfhA1FTPMKMoq', // set this to a recurring price ID
+      quantity: req.body.quantity,
+    //dynamic_tax_rates: ['txr_1HS2ZpICR0NVfhA1n0RiVdCE', 'txr_1HS2QNICR0NVfhA1Ytnl8J3F'],
+    }],
+    // subscription_data:{
+    //     coupon:'oV50lSe0'
+    // }
+  });
+  res.json({
+    id: session.id,
+  });
+});
+
+// Stripe requires the raw body to construct the event
+app.post(
+  "/webhook",
+  bodyParser.raw({ type: "application/json" }),
+  (req, res) => {
+    const sig = req.headers["stripe-signature"];
+
+    let event;
+
+    try {
+      event = stripe.webhooks.constructEvent(req.body, sig, webhookSecret);
+    } catch (err) {
+      // On error, log and return the error message
+      console.log(`❌ Error message: ${err.message}`);
+      return res.status(400).send(`Webhook Error: ${err.message}`);
+    }
+
+    // Successfully constructed event
+    console.log("✅ Success:", event.id);
+
+    if(event.type == 'checkout.session.completed') {
+      const session = event.data.object;
+      console.log('Checkout Session Completed for: ', session.id)
+      console.log('Checkout Session Completed subscription: ', session.subscription)
+      console.log('Checkout Session Completed customer: ', session.customer)
+    }
+
+    // Return a response to acknowledge receipt of the event
+    res.json({ received: true });
+  }
+);
+
+app.listen(4242, () => console.log(`Node server listening on port ${4242}!`));
