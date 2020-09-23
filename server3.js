f0a2288a17597295d7f4864130957bc96a2b3b13
naveenverma1 <01naveenv@gmail.com>
2020-09-23 19:22:12 +0530

    update

--- server3.js
+++ server3.js
@@ -36,25 +36,47 @@ const calculateOrderAmount = items => {
 };
 
 app.post("/pay", async (req, res) => {
+   
   const { token, items } = req.body;
 
   const orderAmount = calculateOrderAmount(items);
-
+  var customer=stripe.customers.create({
+    email: 'customer@example.com',
+  })
   try {
     // Create a charge with the token sent by the client
     const charge = await stripe.charges.create({
+        customer:customer,
       amount: orderAmount,
       currency: "inr",
       source: token
-    });
-
+    })
+   // const stripe = require('stripe')('sk_test_51HNXcdICR0NVfhA1jTSZ93s3hsPX8L1uQsDBeRkK9mkzUtn44srebA18czdMvBiJVo7infozLjVc0TCsBepQED7h003VRwixxQ');
+   const cards = await stripe.customers.listSources(
+    'cus_I4hhpWAAlwXb4t',
+    {object: 'card', limit: 3}
+  );
+  console.log(JSON.stringify(cards)+"%%%%%%%%%%%%%%%%%%      card details")
+console.log(charge)
     // That's it! You're done! The payment was processed 
-    res.send(charge);
+    res.send(charge)
   } catch (e) {
     // Handle "hard declines" e.g. insufficient funds, expired card, etc
     // See https://stripe.com/docs/declines/codes for more
     res.send({ error: e.message });
   }
 });
+// const card =  stripe.customers.retrieveSource(
+//     'cus_I4fg2AAUR8PwD0',
+//   'card_1HUXg5ICR0NVfhA1jMpLSvfO'
+  
+// );
+// {const card =  await stripe.customers.retrieveSource(
+
+//     // 'cus_I4hWUcyTbNWGhT',
+//  card= charge.source.id
+//     // 'card_1HUY7bICR0NVfhA1wWwocL5L'
+//    );
+//    console.log(card+"%%%%%%%%%%%%%%%%%%      card details");}
 
 app.listen(4242, () => console.log(`Node server listening on port ${4242}!`));
